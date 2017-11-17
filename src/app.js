59e85bc19b7f6fb4418df33aedeb196900f90aa1
Giacomo Carnovalini <giacomo.carnovalini@gmail.com>
2017-11-17 19:19:32 +0000

    added sample text on pageload

--- src/app.js
+++ src/app.js
@@ -14,5 +14,8 @@ const app = new Vue({
     markedText: function(text) {
       return marked(text);
     }
+  },
+  mounted: function() {
+    this.inputText = "# Big Header\n\n### Smaller Header\n\nEmphasis: **bold**, *italic*, `monospace`, ~~strikethrough~~\n\nUnordered List:\n* Item 1\n* Item 2\n\nOrdered List:\n1. Item 1\n2. Item 2\n\nCoded by [BleckJeck](https://www.freecodecamp.org/bleckjeck)"
   }
 });
